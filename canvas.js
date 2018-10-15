232063fe3271f95060b5b82f5ce7603fe26bbd85
coruscar <coruscar@gmail.com>
2018-10-15 18:33:06 +0900

    rewrote update into it's own function
    added base framework for hitdetection

--- canvas.js
+++ canvas.js
@@ -2,6 +2,9 @@ var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+// canvas.onmousedown = myDown;
+// canvas.onmouseup = myUp;
+// canvas.ondblclick = myDblClick;
 
 
 var c = canvas.getContext('2d');
@@ -10,7 +13,8 @@ var c = canvas.getContext('2d');
 // console.log(canvas)
 var mouse = {
     x: undefined,
-    y: undefined
+    y: undefined,
+    click: undefined
 }
 
 window.addEventListener('mousemove', 
@@ -20,6 +24,8 @@ window.addEventListener('mousemove',
     mouse.y = event.y;
 })
 
+
+
 class Circle {
     constructor(x,y,dx,dy,radius){
         this.x = x;
@@ -27,7 +33,10 @@ class Circle {
         this.dx = dx;
         this.dy = dy;
         this.radius = radius;
+        // this.l10 = [3,3,3,3,3,3,3,3,3,3];
+        // var ovel = [this.dx,this.dy]
     }
+
     draw(){
         c.beginPath();
         c.arc(this.x,this.y,this.radius,0, Math.PI * 2, false);
@@ -36,33 +45,38 @@ class Circle {
         c.fillStyle = '#b57edc';
         c.fill();
     }
+}
+class Rectangle {
+    constructor(x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        // this.dx = dx;
+        // this.dy = dy;
+    }
+    draw(){
+        c.beginPath();
+        c.rect(this.x, this.y, this.w, this.h)
+        // c.strokeStyle = 'black';
+        // c.stroke();
+        c.fillStyle = 'grey';
+        c.fill();
+    }
     update(){
-        if (this.x + this.radius > innerWidth || this.x - this.radius < 0){
-            this.dx = -this.dx;
-        }
-        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-            this.dy = -this.dy;
-        }
-        this.x += this.dx;
-        this.y += this.dy;
-
-        //mouse functionality
-        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-            if (this.radius < 40){
-                this.radius += 1; 
-
-            }
-        } else if (this.radius > radius){
-            this.radius -= 1;
-        }
     }
 }
 
 var circle = new Circle(200,200,3,3,30);
-
 var circleArr = [];
 
-  
+//first rectangle used as ground for testing
+var rectArr = [];
+rectArr.push(rectangle = new Rectangle(0,canvas.height-50,canvas.width,50));
+// rectArr.push(rectangle = new Rectangle(0,canvas.height,canvas.width,50));
+
+
+
 for (var i = 0; i < 10; i++){
     var radius = 30;
     var x = Math.random() * (innerWidth - radius * 2) + radius;
@@ -70,18 +84,76 @@ for (var i = 0; i < 10; i++){
     var dx = (Math.random() - 0.5 ) * 8;
     var dy = (Math.random() - 0.5 ) * 8;
     circleArr.push(new Circle(x,y,dx,dy,radius));
+    //init array of previous values
 }
 
 console.log(circleArr);
 
+function hitdetection(i){
+    for (var j = 0; j < rectArr.length-1; j++){
+        // console.log("hiteded2")
+        // if (circleArr[i].x + circleArr[i].radius > (rectArr[j].x + rectArr[j].width) || circleArr[i].x - circleArr[i].radius < 0){
+        //     circleArr[i].dx = -circleArr[i].dx;
+        // }
+        // console.log("triggereed")
+        if (circleArr[i].y + circleArr[i].radius > (rectArr[j].y) || circleArr[i].y - circleArr[i].radius < 0) {
+            // console.log("rigget")
+            circleArr[i].dy = -circleArr[i].dy;
+        }
+    }
+}
+
+console.log(circleArr[0].x)
+
+function update(){
+    for (var i = 0; i < circleArr.length; i++){
+
+        // circleArr[i].l10.push(circleArr[i].dx);
+        // circleArr[i].l10.push(circleArr[i].dy);
+        // circleArr[i].l10.shift();
+        // circleArr[i].l10.shift();
+
+        if (circleArr[i].x + circleArr[i].radius > innerWidth || circleArr[i].x - circleArr[i].radius < 0){
+            // console.log("What's going on: " + circleArr[i].l10[5])
+            circleArr[i].dx = -circleArr[i].dx;
+        }
+        if (circleArr[i].y + circleArr[i].radius > innerHeight || circleArr[i].y - circleArr[i].radius < 0) {
+            circleArr[i].dy = -circleArr[i].dy;
+        }
+        circleArr[i].x += circleArr[i].dx;
+        circleArr[i].y += circleArr[i].dy;
+        // circleArr[i].l10x.push(circleArr[i].x);
+        // if (circleArr[i].y + circleArr[i].radius < rectArr[i].x )
+
+        //mouse functionality
+        if (mouse.x - circleArr[i].x < 50 && mouse.x - circleArr[i].x > -50 && mouse.y - circleArr[i].y < 50 && mouse.y - circleArr[i].y > -50) {
+            if (circleArr[i].radius < 40){
+                circleArr[i].radius += 1; 
+
+            }
+        } else if (circleArr[i].radius > radius){
+            circleArr[i].radius -= 1;
+        }
+    }
+}
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0,0,innerWidth,innerHeight);
-
+    update();
     for(var i = 0; i < circleArr.length; i++){
-        circleArr[i].update();
         circleArr[i].draw();
+        // hitdetection(i);
+        // rectangle.update();
+    }
+    for (var i = 0; i < rectArr.length; i++){
+        rectArr[i].draw();
     }
 }
+
+// for(var i = 0; i < 4; i++){
+//     circleArr[i].l10.push(4);
+//     circleArr[i].l10.push(4);
+// }  
 
 animate();
